5e0766fad3ca961f4324e2c0a161138d12b9d785
happiness <makenahappiness97@gmail.com>
2021-11-28 14:30:59 +0300

    added options

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ $(document).ready(function(){
         }
     })
 });
+const sizeSelect = document.getElementById("size");
+const crustSelect = document.getElementById("crust");
+const toppingsSelect = document.getElementById("toppings");
+const numberSelect = document.getElementById("number");
+const deliverySelect = document.getElementById("delivery");
+
+const size = ["Large", "Medium", "Small"];
+const crust = ["Crispy", "Stuffed", "Glutten-free"];
+const toppings = ["Deluxe", "Hawaiian", "Boerewors", "Pepperoni", "Peri peri", "Cheese Burger"];
+const delivery = ["Yes", "No"];
+
+(function populateSize(){
+    for(let i = 0; i < size.length; i++){
+        const option = document.createElement("option");
+        option.textContent = size[i];
+        sizeSelect.appendChild(option);
+    }
+    sizeSelect.value = "Large";
+})();
